8de33157687a600589a3b7b81e0f4d04b4bb829e
haowanjin <2395680185@qq.com>
2021-05-26 11:18:24 +0800

    处理TODO

--- netty-study/src/main/java/com/ddup/netty/reactor/multy/MultiThreadEchoServerReactor.java
+++ netty-study/src/main/java/com/ddup/netty/reactor/multy/MultiThreadEchoServerReactor.java
@@ -31,7 +31,7 @@ public class MultiThreadEchoServerReactor {
 
         // 第一个选择器，负责健康新连接事件
         SelectionKey sk = serverSocketChannel.register(selectors[0], SelectionKey.OP_ACCEPT);
-        // 绑定新连接监控 handler 处理器到 SelectionKey TODO
+        // 绑定新连接监控 handler 处理器到 SelectionKey
         sk.attach(new AcceptorHandler());
         // 第一个子反应器，一个子反应器负责一个选择器
         SubReactor subReactor1 = new SubReactor(selectors[0]);
