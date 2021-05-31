fe23eed2cbaaae8b85f8f0d2fa99a277c41f08a1
haowanjin <2395680185@qq.com>
2021-05-31 20:11:41 +0800

    观察者模式

---
+++ design-pattern/src/com/ddup/pattern/observer/Observer.java
@@ -0,0 +1,14 @@
+package com.ddup.pattern.observer;
+
+/**
+ * 抽象观察类
+ */
+interface Observer {
+    public String getName();
+
+    public void setName(String name);
+
+    public void help(); //声明支援盟友方法  
+
+    public void beAttacked(AllyControlCenter acc); //声明遭受攻击方法
+}  
